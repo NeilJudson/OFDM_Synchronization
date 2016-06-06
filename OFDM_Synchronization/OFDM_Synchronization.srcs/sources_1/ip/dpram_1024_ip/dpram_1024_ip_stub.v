1c9f7c15c2153b0ca260a012b837cd4c450e7b35
NeilJudson <shuiyoufeng@yeah.net>
2016-06-06 23:46:42 +0800

    data_dpram模块调用dpram_1024_ip IP核

---
+++ OFDM_Synchronization/OFDM_Synchronization.srcs/sources_1/ip/dpram_1024_ip/dpram_1024_ip_stub.v
@@ -0,0 +1,26 @@
+// Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2015.2.1 (win64) Build 1302555 Wed Aug  5 13:06:02 MDT 2015
+// Date        : Mon Jun 06 23:18:05 2016
+// Host        : FUTURE2 running 64-bit Service Pack 1  (build 7601)
+// Command     : write_verilog -force -mode synth_stub {g:/jiaweiwei/FPGA
+//               workspace/OFDM_Synchronization/OFDM_Synchronization/OFDM_Synchronization.srcs/sources_1/ip/dpram_1024_ip/dpram_1024_ip_stub.v}
+// Design      : dpram_1024_ip
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z045fbg676-3
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_2,Vivado 2015.2.1" *)
+module dpram_1024_ip(clka, wea, addra, dina, clkb, addrb, doutb)
+/* synthesis syn_black_box black_box_pad_pin="clka,wea[0:0],addra[9:0],dina[31:0],clkb,addrb[9:0],doutb[31:0]" */;
+  input clka;
+  input [0:0]wea;
+  input [9:0]addra;
+  input [31:0]dina;
+  input clkb;
+  input [9:0]addrb;
+  output [31:0]doutb;
+endmodule
---
+++ OFDM_Synchronization/OFDM_Synchronization.srcs/sources_1/ip/dpram_1024_ip/dpram_1024_ip_stub.vhdl
@@ -0,0 +1,36 @@
+-- Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2015.2.1 (win64) Build 1302555 Wed Aug  5 13:06:02 MDT 2015
+-- Date        : Mon Jun 06 23:18:05 2016
+-- Host        : FUTURE2 running 64-bit Service Pack 1  (build 7601)
+-- Command     : write_vhdl -force -mode synth_stub {g:/jiaweiwei/FPGA
+--               workspace/OFDM_Synchronization/OFDM_Synchronization/OFDM_Synchronization.srcs/sources_1/ip/dpram_1024_ip/dpram_1024_ip_stub.vhdl}
+-- Design      : dpram_1024_ip
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z045fbg676-3
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity dpram_1024_ip is
+  Port ( 
+    clka : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addra : in STD_LOGIC_VECTOR ( 9 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    clkb : in STD_LOGIC;
+    addrb : in STD_LOGIC_VECTOR ( 9 downto 0 );
+    doutb : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end dpram_1024_ip;
+
+architecture stub of dpram_1024_ip is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,wea[0:0],addra[9:0],dina[31:0],clkb,addrb[9:0],doutb[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_2,Vivado 2015.2.1";
+begin
+end;
