713bb82a00e75f90825f43a949a7fbb2c4e0bf47
NeilJudson <shuiyoufeng@yeah.net>
2016-07-22 22:16:40 +0800

    更新
    更新

--- Matlab/ZCSequence/AllZC32Sequences.m
+++ Matlab/ZCSequence/AllZC32Sequences.m
@@ -1,7 +1,8 @@
-allZC32Sequences= zeros(32,32,32);
+close all; clear; clc;
+allZC32Sequences = zeros(32,32,32);
 for m = 1:1:32
     for shift = 1:1:32
         allZC32Sequences(shift,:,m) = ZC(32,m,shift-1);
     end
 end
-save('allZC32Sequences');
+save('allZC32Sequences','allZC32Sequences');
