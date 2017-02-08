9856b61910f2e64a5e19393d29f63d53326599ed
NeilJudson <shuiyoufeng@yeah.net>
2017-02-08 14:23:40 +0800

    更新

--- OFDM_Synchronization/OFDM_Synchronization.srcs/sources_1/ip/complex_abs_power2_18_ip/complex_abs_power2_18_ip_stub.v
+++ OFDM_Synchronization/OFDM_Synchronization.srcs/sources_1/ip/complex_abs_power2_18_ip/complex_abs_power2_18_ip_stub.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2015.2.1 (win64) Build 1302555 Wed Aug  5 13:06:02 MDT 2015
-// Date        : Wed Feb 08 11:24:15 2017
-// Host        : FUTURE3 running 64-bit Service Pack 1  (build 7601)
+// Date        : Tue Jan 10 19:43:04 2017
+// Host        : txjs-130 running 64-bit Service Pack 1  (build 7601)
 // Command     : write_verilog -force -mode synth_stub
-//               F:/jiaweiwei/Project/OFDM_Synchronization/OFDM_Synchronization/OFDM_Synchronization.srcs/sources_1/ip/complex_abs_power2_18_ip/complex_abs_power2_18_ip_stub.v
+//               e:/jiaweiwei/Project/OFDM_Synchronization/OFDM_Synchronization.srcs/sources_1/ip/complex_abs_power2_18_ip/complex_abs_power2_18_ip_stub.v
 // Design      : complex_abs_power2_18_ip
 // Purpose     : Stub declaration of top-level module interface
 // Device      : xc7z045fbg676-3
--- OFDM_Synchronization/OFDM_Synchronization.srcs/sources_1/ip/complex_abs_power2_35_1dsp_ip/complex_abs_power2_35_1dsp_ip_stub.v
+++ OFDM_Synchronization/OFDM_Synchronization.srcs/sources_1/ip/complex_abs_power2_35_1dsp_ip/complex_abs_power2_35_1dsp_ip_stub.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2015.2.1 (win64) Build 1302555 Wed Aug  5 13:06:02 MDT 2015
-// Date        : Wed Feb 08 11:25:08 2017
-// Host        : FUTURE3 running 64-bit Service Pack 1  (build 7601)
+// Date        : Thu Jan 12 14:40:50 2017
+// Host        : txjs-130 running 64-bit Service Pack 1  (build 7601)
 // Command     : write_verilog -force -mode synth_stub
-//               F:/jiaweiwei/Project/OFDM_Synchronization/OFDM_Synchronization/OFDM_Synchronization.srcs/sources_1/ip/complex_abs_power2_35_1dsp_ip/complex_abs_power2_35_1dsp_ip_stub.v
+//               e:/jiaweiwei/Project/OFDM_Synchronization/OFDM_Synchronization.srcs/sources_1/ip/complex_abs_power2_35_1dsp_ip/complex_abs_power2_35_1dsp_ip_stub.v
 // Design      : complex_abs_power2_35_1dsp_ip
 // Purpose     : Stub declaration of top-level module interface
 // Device      : xc7z045fbg676-3
